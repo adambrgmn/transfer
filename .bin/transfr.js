6c9b29a13e1aac846b4676a4eac8166fcfdb6ab6
Adam Bergman <adam@fransvilhelm.com>
2017-04-06 01:08:03 +0200

    Add ability to upload multiple files
    This commit gives the CLI the ability to accept a range of files. It will then
    zip the files to a buffer, create a stream and follow along with the others.

--- .bin/transfr.js
+++ .bin/transfr.js
@@ -2,7 +2,9 @@
 
 const path = require('path');
 const program = require('commander');
+const createZip = require('../lib/createZip');
 const checkSize = require('../lib/checkSize');
+const readFile = require('../lib/readFile');
 const putFile = require('../lib/putFile');
 const copy = require('../lib/copy');
 const pkg = require('../package.json');
@@ -10,15 +12,18 @@ const pkg = require('../package.json');
 program
   .version(pkg.version)
   .description(pkg.description)
-  .arguments('<file>')
-  .action((file) => {
-    const filePath = path.resolve(file);
+  .arguments('<files...>')
+  .action((files) => {
+    const filePaths = files.map(file => path.resolve(file));
+    const fileName = path.basename(filePaths.length > 1 ? 'transfer.zip' : filePaths[0]);
 
-    checkSize(filePath)
-      .then(putFile)
+    createZip(filePaths)
+      .then(checkSize)
+      .then(readFile)
+      .then(putFile(fileName))
       .then(copy)
       .then(() => process.exit(0))
-      .catch(() => process.exit(1));
+      .catch(err => process.exit(err.code || 1));
   });
 
 program.parse(process.argv);
