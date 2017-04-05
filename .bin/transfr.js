656e236ace3241b7cc6303c3dc72c02266cbc9b5
Adam Bergman <adam@fransvilhelm.com>
2017-04-05 17:53:08 +0200

    Add initial check of file size
    transfer.sh has a limit of 10GB file size. checkSize.js does a quick check to
    see if the size is okey and then proceeds. Or rejects the promise if not.

--- .bin/transfr.js
+++ .bin/transfr.js
@@ -2,6 +2,7 @@
 
 const path = require('path');
 const program = require('commander');
+const checkSize = require('../lib/checkSize');
 const putFile = require('../lib/putFile');
 const copy = require('../lib/copy');
 const pkg = require('../package.json');
@@ -12,7 +13,9 @@ program
   .arguments('<file>')
   .action((file) => {
     const filePath = path.resolve(file);
-    putFile(filePath)
+
+    checkSize(filePath)
+      .then(putFile)
       .then(copy)
       .then(() => process.exit(0))
       .catch(() => process.exit(1));
